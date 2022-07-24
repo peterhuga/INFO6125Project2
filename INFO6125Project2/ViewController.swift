3e21563ed5670efc7c762e820356e5b05d588b8f
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-07-24 15:18:46 -0400

    Initial Commit

---
+++ INFO6125Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INFO6125Project2
+//
+//  Created by Jason Wang on 2022-07-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
